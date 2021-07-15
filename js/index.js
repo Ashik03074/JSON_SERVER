823a30fc517815c0c59c083e8dca01cc53f5de0b
Ashik03074 <ashik03074@gmail.com>
2021-07-16 02:21:58 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.blogs');
+const renderPosts = async ()=>{
+    let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+    //console.log(posts);
+    let template = '';
+    posts.forEach(post =>{
+        template +=`
+          <div class="post">
+             <h2>${post.title}</h2>
+             <p><small>${post.likes} likes</small></p>
+             <p>${post.body.slice(0,80)}</p>
+             <a href="details.html?id=${post.id}">read more...</a>
+          </div>
+        `
+    })
+
+    container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded',()=>renderPosts());
